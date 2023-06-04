83c3e122272d996e2614323f89b04ac6b3ea4fcf
EugenJun <84986104+EugenJun@users.noreply.github.com>
2023-06-04 20:17:12 +0200

    Added functionality to 3rd menu option(4th & 5th stages)

--- first_stage_commander.sh
+++ first_stage_commander.sh
@@ -16,14 +16,80 @@ show_menu() {
 EOF
 }
 
+show_dir_content() {
+	while true; do	
+		echo -e "The list of files and directories:"
+		arr=(*)	
+		for item in "${arr[@]}"; do
+  			if [[ -f "$item" ]]; then
+    				echo "F $item"
+  			elif [[ -d "$item" ]]; then
+    				echo "D $item"
+  			fi
+		done
+
+		    cat <<EOF
+	
+---------------------------------------------------
+| 0 Main menu | 'up' To parent | 'name' To select |
+---------------------------------------------------
+EOF
+		read -r input
+		case $input in
+       	    		0) main ;;
+    			"up") cd .. && echo "" && show_dir_content ;;
+    			*)
+        		found=false
+        		for item in "${arr[@]}"; do
+            		if [[ "$item" == "$input" ]]; then
+                		found=true
+                		if [[ -d "$input" ]]; then
+                    			cd "$input" && echo "" && show_dir_content
+                		elif [[ -f "$input" ]]; then
+                    			show_file_menu "$input"
+                		fi
+            		fi
+        		done
+        		if [[ $found == false ]]; then
+        			echo "Invalid input!" && continue
+        		fi ;;
+		esac
+	done
+}
+
+show_file_menu() {
+	while true; do
+                    cat <<EOF
+---------------------------------------------------------------------
+| 0 Back | 1 Delete | 2 Rename | 3 Make writable | 4 Make read-only |
+---------------------------------------------------------------------
+EOF
+		read -r input
+		case $input in
+			0) show_dir_content ;;
+			2) echo "Enter the new file name:" && read -r new_name && mv "$1" "$new_name"
+				echo "$1 has been renamed as $new_name" && echo ""
+				show_dir_content ;;
+			1) rm $1 && echo -e "$1 has been deleted.\n" && show_dir_content ;;
+			3) chmod 666 "$1"; echo "Permissions have been updated." && ls -l "$1" && echo "" && show_dir_content ;;
+			4) chmod 664 "$1"; echo "Permissions have been updated." && ls -l "$1" && echo "" && show_dir_content ;;
+			*) show_file_menu "$1" ;;
+		esac
+	done
+}
+
+main() {
 while true; do
 	show_menu && read -r choice && echo ""
 	case $choice in
-		0) echo -e "Farewell!" && break ;;
+		0) echo -e "Farewell!" && exit 1 ;;
 		1) uname -n -o ;;
 		2) whoami ;;
-		[3-4]) echo "Not implemented!" ;;
+		3) show_dir_content ;;
+		4) echo "Not implemented!" ;;
                 *) echo "Invalid option!" ;;
 	esac
 done
+}
 
+main
